c46154d287651fd85c0041336da4e480aa20581e
hrishik18 <hrishik.s@somaiya.edu>
2023-04-12 16:31:40 +0530

    dev server setup

--- truffle/migrations/1_Donation.js
+++ truffle/migrations/1_Donation.js
@@ -1,5 +1,8 @@
 const Donation = artifacts.require("Donation");
 
-export default function (deployer) {
-  deployer.deploy(Donation);
+module.exports = async function (deployer) {
+  //deployer.deploy(Donation);
+  const inst = await Donation.deployed();
+  const result = await inst.getBalance();
+  console.log(result.toNumber());
 };
